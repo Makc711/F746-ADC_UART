2df458275d1bde9ffe2e341f063bf00936fdf0e4
Максим Русанов <g.frimen@mail.ru>
2024-05-29 22:59:47 +0700

    Fixed CRC calculation

--- app/drivers/memory/memory.cpp
+++ app/drivers/memory/memory.cpp
@@ -6,7 +6,6 @@
   */
 
 #include "memory.h"
-#include <algorithm>
 
 memory::memory(const addr start_addr, CRC_HandleTypeDef& hcrc)
   : memory_t()
@@ -16,19 +15,23 @@ memory::memory(const addr start_addr, CRC_HandleTypeDef& hcrc)
 memory::status memory::read_data_from_flash(data_t* full_data) const
 {
   read_arr_from_flash(f_start_addr, reinterpret_cast<uint32_t*>(full_data), sizeof(k_default_val));
-  const uint32_t crc_read = read_word(f_start_addr + sizeof(k_default_val));
+  const uint32_t end_addr = f_start_addr + sizeof(k_default_val);
+  const uint32_t crc_read = read_word(end_addr);
 
-  if (const uint32_t crc_calc = HAL_CRC_Accumulate(&f_hcrc, reinterpret_cast<uint32_t*>(full_data), 
-                                                   sizeof(k_default_val) / sizeof(uint32_t)); 
+  if (const uint32_t crc_calc = HAL_CRC_Calculate(&f_hcrc,
+      reinterpret_cast<uint32_t*>(full_data),
+      sizeof(k_default_val) / sizeof(uint32_t));
       crc_read != crc_calc)
   {
 #if USE_FLASH
-    const uint32_t end_addr = f_start_addr + sizeof(k_default_val);
     erase(f_start_addr, end_addr);
 #endif
     write_arr_to_flash(f_start_addr, reinterpret_cast<const uint32_t*>(k_default_val), sizeof(k_default_val));
-    write_only_one_word(f_start_addr + sizeof(k_default_val), crc_calc);
-    std::copy(std::begin(k_default_val), std::end(k_default_val), full_data);
+    read_arr_from_flash(f_start_addr, reinterpret_cast<uint32_t*>(full_data), sizeof(k_default_val));
+    const uint32_t crc = HAL_CRC_Calculate(&f_hcrc,
+      reinterpret_cast<uint32_t*>(full_data),
+      sizeof(k_default_val) / sizeof(uint32_t));
+    write_only_one_word(end_addr, crc);
     return status::GET_DEFAULT;
   }
   return status::READ_OK;
